8c27df6a6615446f60c175409d7c3a0ae2cdb8e5
tranphuongduy20 <18520038@gm.uit.edu.vn>
2020-12-26 11:20:19 +0700

    fix service api, and re design ui for service

---
+++ src/com/wedding/serviceImpl/ServiceServiceImpl.java
@@ -0,0 +1,48 @@
+package com.wedding.serviceImpl;
+
+import java.util.List;
+
+import com.wedding.models.Service;
+import com.wedding.models.ServicePrice;
+import com.wedding.repository.ServiceRepository;
+import com.wedding.service.ServiceService;
+
+public class ServiceServiceImpl implements ServiceService {
+
+	private ServiceRepository serviceRepository;
+
+	public ServiceServiceImpl() {
+		serviceRepository = new ServiceRepository();
+	}
+	
+	@Override
+	public List<Service> getAllService() {
+		return serviceRepository.getAll();
+	}
+
+	@Override
+	public void addService(Service service) {
+		serviceRepository.add(service);
+
+	}
+
+	@Override
+	public void deleteService(int id) {
+		serviceRepository.delele(id);
+
+	}
+
+	@Override
+	public void updateService(Service service) {
+		serviceRepository.update(service);
+	}
+
+	@Override
+	public List<Service> removeService(List<ServicePrice> listReservedService) {
+		return serviceRepository.removeService(listReservedService);
+	}
+
+	
+
+
+}
