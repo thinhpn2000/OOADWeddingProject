d48f5ecf4d326c822b20289df9a659e88b78f20d
dinhngocup <18520335@gm.uit.edu.vn>
2020-12-30 09:38:06 +0700

    change api to get data

--- src/com/wedding/utils/APIConstant.java
+++ src/com/wedding/utils/APIConstant.java
@@ -2,48 +2,48 @@ package com.wedding.utils;
 
 public class APIConstant {
 	// RESTFUL of API food
-		public static final String API_food_get = "http://localhost:8082/WeddingManagementBackend/food";
-		public static final String API_food_add = "http://localhost:8082/WeddingManagementBackend/food/add";
-		public static final String API_food_delete = "http://localhost:8082/WeddingManagementBackend/food/delete";
-		public static final String API_food_update = "http://localhost:8082/WeddingManagementBackend/food/update";
+		public static final String API_food_get = "http://localhost:8082/OOADWeddingBackendProject/food";
+		public static final String API_food_add = "http://localhost:8082/OOADWeddingBackendProject/food/add";
+		public static final String API_food_delete = "http://localhost:8082/OOADWeddingBackendProject/food/delete";
+		public static final String API_food_update = "http://localhost:8082/OOADWeddingBackendProject/food/update";
 		
 		// RESTFUL of API service
-		public static final String API_service_get = "http://localhost:8082/WeddingManagementBackend/service";
-		public static final String API_service_add = "http://localhost:8082/WeddingManagementBackend/service/add";
-		public static final String API_service_delete = "http://localhost:8082/WeddingManagementBackend/service/delete";
-		public static final String API_service_update = "http://localhost:8082/WeddingManagementBackend/service/update";
+		public static final String API_service_get = "http://localhost:8082/OOADWeddingBackendProject/service";
+		public static final String API_service_add = "http://localhost:8082/OOADWeddingBackendProject/service/add";
+		public static final String API_service_delete = "http://localhost:8082/OOADWeddingBackendProject/service/delete";
+		public static final String API_service_update = "http://localhost:8082/OOADWeddingBackendProject/service/update";
 		
 		// RESTFUL of API lobby
-		public static final String API_lobby_get = "http://localhost:8082/WeddingManagementBackend/lobby";
-		public static final String API_lobby_add = "http://localhost:8082/WeddingManagementBackend/lobby/add";
-		public static final String API_lobby_delete = "http://localhost:8082/WeddingManagementBackend/lobby/delete";
-		public static final String API_lobby_update = "http://localhost:8082/WeddingManagementBackend/lobby/update";
-		public static final String API_lobby_check = "http://localhost:8082/WeddingManagementBackend/lobby/check";
+		public static final String API_lobby_get = "http://localhost:8082/OOADWeddingBackendProject/lobby";
+		public static final String API_lobby_add = "http://localhost:8082/OOADWeddingBackendProject/lobby/add";
+		public static final String API_lobby_delete = "http://localhost:8082/OOADWeddingBackendProject/lobby/delete";
+		public static final String API_lobby_update = "http://localhost:8082/OOADWeddingBackendProject/lobby/update";
+		public static final String API_lobby_check = "http://localhost:8082/OOADWeddingBackendProject/lobby/check";
 		
 		// RESTFUL of API employee
-		public static final String API_employee_get = "http://localhost:8082/WeddingManagementBackend/employee";
-		public static final String API_employee_add = "http://localhost:8082/WeddingManagementBackend/employee/add";
-		public static final String API_employee_delete = "http://localhost:8082/WeddingManagementBackend/employee/delete";
-		public static final String API_employee_update = "http://localhost:8082/WeddingManagementBackend/employee/update";
-		public static final String API_employee_resetpassword = "http://localhost:8082/WeddingManagementBackend/employee/resetpassword";
-		public static final String API_employee_username = "http://localhost:8082/WeddingManagementBackend/employee/username";
+		public static final String API_employee_get = "http://localhost:8082/OOADWeddingBackendProject/employee";
+		public static final String API_employee_add = "http://localhost:8082/OOADWeddingBackendProject/employee/add";
+		public static final String API_employee_delete = "http://localhost:8082/OOADWeddingBackendProject/employee/delete";
+		public static final String API_employee_update = "http://localhost:8082/OOADWeddingBackendProject/employee/update";
+		public static final String API_employee_resetpassword = "http://localhost:8082/OOADWeddingBackendProject/employee/resetpassword";
+		public static final String API_employee_username = "http://localhost:8082/OOADWeddingBackendProject/employee/username";
 		
 		// RESTFUL of API change password
-		public static final String API_employee_changepassword = "http://localhost:8082/WeddingManagementBackend/updatepassword";
+		public static final String API_employee_changepassword = "http://localhost:8082/OOADWeddingBackendProject/updatepassword";
 		
 		// RESTFUL of API typelobby
-		public static final String API_typelobby_get = "http://localhost:8082/WeddingManagementBackend/typelobby";
+		public static final String API_typelobby_get = "http://localhost:8082/OOADWeddingBackendProject/typelobby";
 		
 		// RESTFUL of API reservation
-		public static final String API_reservation = "http://localhost:8082/WeddingManagementBackend/reservation";
-		public static final String API_reservation_add = "http://localhost:8082/WeddingManagementBackend/reservation/add";
-		public static final String API_reservation_delete = "http://localhost:8082/WeddingManagementBackend/reservation/delete";
-		public static final String API_reservation_update = "http://localhost:8082/WeddingManagementBackend/reservation/update";
-		public static final String API_reservation_pay = "http://localhost:8082/WeddingManagementBackend/reservation/pay";
+		public static final String API_reservation = "http://localhost:8082/OOADWeddingBackendProject/reservation";
+		public static final String API_reservation_add = "http://localhost:8082/OOADWeddingBackendProject/reservation/add";
+		public static final String API_reservation_delete = "http://localhost:8082/OOADWeddingBackendProject/reservation/delete";
+		public static final String API_reservation_update = "http://localhost:8082/OOADWeddingBackendProject/reservation/update";
+		public static final String API_reservation_pay = "http://localhost:8082/OOADWeddingBackendProject/reservation/pay";
 		
 		// RESTFUL of API total revenue
-		public static final String API_total_revenue = "http://localhost:8082/WeddingManagementBackend/totalrevenue";
-		public static final String API_month_revenue = "http://localhost:8082/WeddingManagementBackend/monthrevenue";
+		public static final String API_total_revenue = "http://localhost:8082/OOADWeddingBackendProject/totalrevenue";
+		public static final String API_month_revenue = "http://localhost:8082/OOADWeddingBackendProject/monthrevenue";
 		
-		public static final String API_login = "http://localhost:8082/WeddingManagementBackend/login";
+		public static final String API_login = "http://localhost:8082/OOADWeddingBackendProject/login";
 }
